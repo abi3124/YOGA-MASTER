2ac54125187ccdf77643c2d1042d513926c1585a
ABINAYA <155756997+abi3124@users.noreply.github.com>
2025-11-08 22:22:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+  alert("Redirecting to Login Page... (Functionality coming soon)");
+}
+
+function signup() {
+  alert("Redirecting to Sign Up Page... (Functionality coming soon)");
+}
